3c17e57b8f03ef5a10b63ffab2f97e4bb3c9330e
simajune <tejayjung@gmail.com>
2019-04-12 02:17:53 +0900

    - 모델 추가

--- JoyfulChurch/HomeController/Models/Message.swift
+++ JoyfulChurch/HomeController/Models/Message.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 struct Message {
+    var title: String?
+    var date: Date?
+    var speaker: String?
+    var bible: String?
     
+    init(dic: [String: Any]) {
+        guard let title = dic["title"] as? String else { return }
+        self.title = title
+        guard let date = dic["date"] as? Date else { return }
+        self.date = date
+        guard let speaker = dic["speaker"] as? String else { return }
+        self.speaker = speaker
+        guard let bible = dic["bible"] as? String else { return }
+        self.bible = bible
+    }
 }
