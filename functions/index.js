02a2258c0743abd4b5055f7a867d9fe02c85aa35
정태준 <tejayjung@gmail.com>
2020-02-17 17:34:33 +0900

    Update firebase config

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.version = functions.https.onRequest((request, response) => {
+
+    response.send({
+        "version": "1.0.0",
+        "strongVersion":"1.0.0",
+        "weakVersion":"1.0.0"
+    })
+});
